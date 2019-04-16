92b6014a8fc1e49323396495569d2562c11c053d
Jay Boyd <jaboyd@redhat.com>
2019-04-16 14:48:26 -0400

    get pod failure msg from log

--- pkg/operator/v311_00_assets/bindata.go
+++ pkg/operator/v311_00_assets/bindata.go
@@ -551,6 +551,7 @@ spec:
         imagePullPolicy: IfNotPresent
         command: ["/usr/bin/service-catalog"]
         name: apiserver
+        terminationMessagePolicy: FallbackToLogsOnError
         ports:
         - containerPort: 6443
           protocol: TCP
